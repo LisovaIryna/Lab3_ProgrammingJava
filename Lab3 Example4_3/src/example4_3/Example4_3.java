c2444e635f932a61ec83a2a51c778531173bf923
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2022-04-07 18:31:11 +0300

    Example 4.3

---
+++ Lab3 Example4_3/src/example4_3/Example4_3.java
@@ -0,0 +1,21 @@
+package example4_3;
+
+public class Example4_3 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("ошибка");
+        }
+        catch (NullPointerException e) {
+            System.out.println("1" );
+        }
+        catch (RuntimeException e) {
+            System.out.println("2" );
+        }
+        catch (Exception e) {
+            System.out.println("3" );
+        }
+        System.out.println("4");
+    }
+}
