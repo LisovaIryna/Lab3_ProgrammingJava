80975934760df02876428454adccebda5b763510
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2022-04-07 18:42:21 +0300

    Example 4.6

---
+++ Lab3 Example4_6/src/example4_6/Example4_6.java
@@ -0,0 +1,21 @@
+package example4_6;
+
+public class Example4_6 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("1" );
+        }
+        catch (Exception e) {
+            System.out.println("2" );
+        }
+        catch (RuntimeException e) {
+            System.out.println("3" );
+        }
+        System.out.println("4");
+    }
+}
