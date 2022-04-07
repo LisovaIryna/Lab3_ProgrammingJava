e0ee1a9c8f4481ac81311ef2eeb7534cb64b3762
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2022-04-07 18:46:52 +0300

    Example 4.8

---
+++ Lab3 Example4_8/src/example4_8/Example4_8.java
@@ -0,0 +1,17 @@
+package example4_8;
+
+public class Example4_8 {
+    public static int m() {
+        try {
+            System.out.println("0");
+            throw new RuntimeException();
+        }
+        finally {
+            System.out.println("1");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(m());
+    }
+}
